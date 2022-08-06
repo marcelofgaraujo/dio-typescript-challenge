8e110b19e8f86bb945f1ddf12920bd9926fa9ed6
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-08-06 13:09:59 -0300

    fix type of elements

---
+++ src/challenge-3/challenge-3.ts
@@ -0,0 +1,31 @@
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
+
+let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLSpanElement;
+
+let saldoTotal = 0
+
+function somarAoSaldo(soma: number) {
+    saldoTotal += soma
+    campoSaldo.innerHTML += saldoTotal.toString()
+}
+
+function limparSaldo() {
+    saldoTotal = 0;
+    campoSaldo.innerHTML = saldoTotal.toString();
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
